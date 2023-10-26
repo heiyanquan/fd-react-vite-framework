6981f81f14aa4c0ee8d28246282c81544fb1b08b
heiyanquan <2792652898@qq.com>
2023-10-26 22:57:06 +0800

    Table

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,29 +1,95 @@
-import { FC, useState, useEffect } from 'react'
-import { Button, Spin } from 'antd'
+import { FC, useMemo, useState } from 'react'
+import { Button, Space, Table, Flex } from 'antd'
+import { useNavigate } from 'react-router-dom'
+import './style.less'
 
 const HomePage: FC = () => {
-  const [status, setStatus] = useState('loading')
-  const onChange = () => {
-    setStatus('loading')
-    setTimeout(() => {
-      setStatus('success')
-    }, 3000)
-  }
+  const navigate = useNavigate()
 
-  useEffect(() => {
-    setTimeout(() => {
-      setStatus('success')
-    }, 3000)
-  }, [])
+  const [data, setdata] = useState(
+    new Array(500)
+      .fill({
+        name: 'John Brown',
+        age: 32,
+        address: 'New York No. 1 Lake Park',
+        tags: ['nice', 'developer']
+      })
+      .map((item, index) => ({
+        ...item,
+        name: `${item.name}${index}`
+      }))
+  )
+  const columns = useMemo(
+    () => [
+      {
+        title: 'Name',
+        dataIndex: 'name'
+      },
+      {
+        title: 'Age',
+        dataIndex: 'age'
+      },
+      {
+        title: 'Address',
+        dataIndex: 'address'
+      },
+      {
+        title: 'Tags',
+        dataIndex: 'tags'
+      }
+    ],
+    []
+  )
+
+  const toPage = () => {
+    navigate('/router?user=name1', {
+      state: {
+        params: {
+          id: 1
+        }
+      }
+    })
+  }
 
   return (
     <>
-      {status === 'loading' && <Spin />}
-      {status === 'success' && (
-        <Button type="primary" onClick={onChange}>
-          Primary Button
+      <Space wrap>
+        <Button type="primary" onClick={toPage}>
+          to router
         </Button>
-      )}
+      </Space>
+      <Flex gap={54}>
+        <table>
+          <thead>
+            <tr>
+              {columns.map((item) => (
+                <td key={item.title}>{item.title}</td>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item) => (
+              <tr key={item.name}>
+                <td>
+                  <input type="checkbox" />
+                </td>
+                <td>{item.name}</td>
+                <td>{item.age}</td>
+                <td>{item.address}</td>
+                <td>{item.tags}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <Table
+          rowSelection={{
+            type: 'checkbox'
+          }}
+          columns={columns}
+          dataSource={data}
+          pagination={false}
+          rowKey="name"></Table>
+      </Flex>
     </>
   )
 }
