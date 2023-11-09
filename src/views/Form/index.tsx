064e69064912d8043e75fea39f37854a691576da
heiyanquan <2792652898@qq.com>
2023-11-09 17:43:51 +0800

    Form.List

--- src/views/Form/index.tsx
+++ src/views/Form/index.tsx
@@ -1,179 +1,47 @@
-import { Form, Button, Space } from 'antd'
-import { useState, memo, useEffect } from 'react'
-import { getPlanningDataSheetDetail } from '@/api/planningOverview/dataSheet'
-import { getPlanningSubclassList } from '@/api/planningOverview/subclass'
-import { getDataSheetAttributes, getDataSheetPlanningDimension, getDataMaintenanceMode } from '@/utils/type'
+import { Form, Button, Input, Space } from 'antd'
+import { FC } from 'react'
 import './style.less'
-import SelectUsers from './SelectUsers'
-import HsAdminForm from '@/components/HsAdminForm'
+import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons'
 
-const FormPage: React.FC = () => {
+const FormPage: FC = () => {
   const [form] = Form.useForm()
-  const [formValue, setformValue] = useState<any>({})
-  const [userOptions, setuserOptions] = useState<any[]>([])
-  const [formItemOptions, setformItemOptions] = useState<any[]>([
-    {
-      label: '表名',
-      name: 'en_table_name',
-      component: 'Input',
-      required: true,
-      FormItemColProps: {
-        span: 12
-      }
-    },
-    {
-      label: '中文名',
-      name: 'cn_table_name',
-      component: 'TextArea',
-      required: true,
-      FormItemColProps: {
-        span: 12
-      }
-    },
-    {
-      label: '所属数据子类',
-      name: 'sub_topic_domain_id',
-      component: 'Select',
-      required: true,
-      componentProps: {
-        options: [],
-        'label-field': 'name',
-        'value-field': 'id'
-      },
-      FormItemColProps: {
-        span: 12
-      }
-    },
-    {
-      label: '表类型',
-      name: 'attributes',
-      component: 'Select',
-      required: true,
-      componentProps: {
-        options: getDataSheetAttributes().slice(1)
-      },
-      FormItemColProps: {
-        span: 12
-      }
-    },
-    {
-      label: '规划负责人',
-      name: 'plan_lead_id',
-      required: true,
-      slot: 'plan_lead_id',
-      FormItemColProps: {
-        span: 12
-      }
-    },
-    {
-      label: '预计数据量',
-      name: 'expected_data_volume',
-      component: 'InputNumber',
-      FormItemColProps: {
-        span: 12
-      }
-    },
-    {
-      label: '维护方式',
-      name: 'maintenance_method',
-      component: 'Select',
-      required: true,
-      componentProps: {
-        options: getDataMaintenanceMode(),
-        multiple: true
-      },
-      FormItemColProps: {
-        span: 12
-      }
-    },
-    {
-      label: '规划维度',
-      name: 'planning_dimension',
-      component: 'Select',
-      required: true,
-      componentProps: {
-        options: getDataSheetPlanningDimension()
-      },
-      FormItemColProps: {
-        span: 12
-      }
-    },
-    {
-      label: '',
-      name: 'submit',
-      slot: 'submit',
-      FormItemColProps: {
-        span: 24
-      },
-      FormItemProps: {
-        wrapperCol: { offset: 16 }
-      }
-    }
-  ])
-
   const onFinish = (values: any) => {
-    const formValues = form.getFieldsValue()
-    console.log('formValues', values, formValues)
+    console.log('Received values of form:', values)
+    console.log('form:', form.getFieldsValue())
   }
-  const onFinishFailed = (errorInfo: any) => {
-    console.log('Failed:', errorInfo)
-  }
-
-  const onChange = (value: string) => {
-    console.log('onChange:', value)
-  }
-  const onReset = () => {
-    form.resetFields()
-  }
-  const onFill = () => {
-    form.setFieldsValue(formValue)
-    setuserOptions([
-      {
-        label: formValue.plan_lead,
-        value: formValue.plan_lead_id
-      }
-    ])
-  }
-
-  useEffect(() => {
-    getPlanningSubclassList({
-      page: 1,
-      page_size: 100000
-    }).then((res) => {
-      const subclassOptions = res.items.map((item: any) => ({
-        label: item.name,
-        value: item.id
-      }))
-      setformItemOptions((options) => {
-        options[2].componentProps.options = subclassOptions
-        return [...options]
-      })
-    })
-    getPlanningDataSheetDetail({ id: 1 }).then((res) => {
-      setformValue(res)
-    })
-  }, [])
 
   return (
-    <HsAdminForm formItemOptions={formItemOptions} form={form} onFinish={onFinish} onFinishFailed={onFinishFailed}>
-      <div slot="plan_lead_id">
-        <SelectUsers placeholder="请选择规划负责人" onChange={onChange} options={userOptions}></SelectUsers>
-      </div>
-      <div slot="submit">
-        <Space>
-          <Button type="primary" htmlType="submit">
-            提交
-          </Button>
-          <Button htmlType="button" onClick={onReset}>
-            重置
-          </Button>
-          <Button type="link" htmlType="button" onClick={onFill}>
-            填充表单
-          </Button>
-        </Space>
-      </div>
-    </HsAdminForm>
+    <Form form={form} onFinish={onFinish}>
+      <Form.List name="users">
+        {(fields, { add, remove }) => (
+          <>
+            {console.log('[ fields ] >', fields)}
+            {fields.map(({ key, name, ...restField }) => (
+              <Space key={key} style={{ display: 'flex', marginBottom: 8 }} align="baseline">
+                <Form.Item {...restField} name={[name, 'first']} rules={[{ required: true, message: 'Missing first name' }]}>
+                  <Input placeholder="First Name" />
+                </Form.Item>
+                <Form.Item {...restField} name={[name, 'last']} rules={[{ required: true, message: 'Missing last name' }]}>
+                  <Input placeholder="Last Name" />
+                </Form.Item>
+                <MinusCircleOutlined onClick={() => remove(name)} />
+              </Space>
+            ))}
+            <Form.Item>
+              <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined />}>
+                Add field
+              </Button>
+            </Form.Item>
+          </>
+        )}
+      </Form.List>
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
   )
 }
 
-export default memo(FormPage)
+export default FormPage
--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,45 +1,11 @@
-import { FC, useMemo, useState } from 'react'
-import { Button, Space, Table, Flex } from 'antd'
+import { FC, useEffect } from 'react'
+import { Button, Space } from 'antd'
 import { useNavigate } from 'react-router-dom'
 import './style.less'
+import { initChart } from './chart'
 
 const HomePage: FC = () => {
   const navigate = useNavigate()
-
-  const [data, setdata] = useState(
-    new Array(500)
-      .fill({
-        name: 'John Brown',
-        age: 32,
-        address: 'New York No. 1 Lake Park',
-        tags: ['nice', 'developer']
-      })
-      .map((item, index) => ({
-        ...item,
-        name: `${item.name}${index}`
-      }))
-  )
-  const columns = useMemo(
-    () => [
-      {
-        title: 'Name',
-        dataIndex: 'name'
-      },
-      {
-        title: 'Age',
-        dataIndex: 'age'
-      },
-      {
-        title: 'Address',
-        dataIndex: 'address'
-      },
-      {
-        title: 'Tags',
-        dataIndex: 'tags'
-      }
-    ],
-    []
-  )
 
   const toPage = () => {
     navigate('/router?user=name1', {
@@ -51,6 +17,10 @@ const HomePage: FC = () => {
     })
   }
 
+  useEffect(() => {
+    initChart()
+  }, [])
+
   return (
     <>
       <Space wrap>
@@ -58,38 +28,7 @@ const HomePage: FC = () => {
           to router
         </Button>
       </Space>
-      <Flex gap={54}>
-        <table>
-          <thead>
-            <tr>
-              {columns.map((item) => (
-                <td key={item.title}>{item.title}</td>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((item) => (
-              <tr key={item.name}>
-                <td>
-                  <input type="checkbox" />
-                </td>
-                <td>{item.name}</td>
-                <td>{item.age}</td>
-                <td>{item.address}</td>
-                <td>{item.tags}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <Table
-          rowSelection={{
-            type: 'checkbox'
-          }}
-          columns={columns}
-          dataSource={data}
-          pagination={false}
-          rowKey="name"></Table>
-      </Flex>
+      <div className="chart" id="main"></div>
     </>
   )
 }
