784b0430846d3cbb5be0848cc19272c9838cc2ea
heiyanquan <2792652898@qq.com>
2023-11-28 19:17:42 +0800

    CodeMirror

--- src/views/MonacoEditor/index.tsx
+++ src/views/MonacoEditor/index.tsx
@@ -2,25 +2,38 @@ import { FC, SetStateAction, useCallback, useState } from 'react'
 import CodeMirror from '@uiw/react-codemirror'
 import { javascript } from '@codemirror/lang-javascript'
 import { Select } from 'antd'
+import { markdown, markdownLanguage } from '@codemirror/lang-markdown'
+import { languages } from '@codemirror/language-data'
 
 const MonacoEditor: FC = () => {
   const [value, setValue] = useState("console.log('hello world!');")
-  const options = useState([
+  const [selectValue, setselectValue] = useState('javascript')
+  const options = [
     { label: 'javascript', value: 'javascript' },
     { label: 'sql', value: 'sql' },
     { label: 'json', value: 'json' },
     { label: 'markdown', value: 'markdown' },
     { label: 'python', value: 'python' }
-  ])
+  ]
   const [extensions, setextensions] = useState([javascript({ jsx: true })])
 
   const selectChange = (value: any) => {
     console.log('[ value ] >', value)
+    setselectValue(value)
     switch (value) {
       case 'javascript':
+        setValue("console.log('hello world!');")
         setextensions([javascript({ jsx: true })])
         break
-
+      case 'markdown':
+        setValue(`
+          # Not dependent on uiw.
+          npm install @codemirror/lang-markdown --save
+          npm install @codemirror/language-data --save
+          [weisit ulr](https://uiwjs.github.io/react-codemirror/)
+        `)
+        setextensions([markdown({ base: markdownLanguage, codeLanguages: languages })])
+        break
       default:
         break
     }
@@ -31,7 +44,7 @@ const MonacoEditor: FC = () => {
   }, [])
   return (
     <>
-      <Select options={options} onChange={selectChange}></Select>
+      <Select value={selectValue} options={options} onChange={selectChange}></Select>
       <CodeMirror value={value} height="200px" extensions={extensions} onChange={onChange} />
     </>
   )
