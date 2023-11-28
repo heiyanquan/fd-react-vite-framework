8727b8c6784c1f6743763a5f9638534bf1ee8a72
heiyanquan <2792652898@qq.com>
2023-11-28 18:49:47 +0800

    language

--- src/views/MonacoEditor/index.tsx
+++ src/views/MonacoEditor/index.tsx
@@ -1,16 +1,38 @@
-import { FC, useCallback, useState } from 'react'
+import { FC, SetStateAction, useCallback, useState } from 'react'
 import CodeMirror from '@uiw/react-codemirror'
 import { javascript } from '@codemirror/lang-javascript'
+import { Select } from 'antd'
 
 const MonacoEditor: FC = () => {
   const [value, setValue] = useState("console.log('hello world!');")
-  const onChange = useCallback((val, viewUpdate) => {
-    console.log('val:', val)
+  const options = useState([
+    { label: 'javascript', value: 'javascript' },
+    { label: 'sql', value: 'sql' },
+    { label: 'json', value: 'json' },
+    { label: 'markdown', value: 'markdown' },
+    { label: 'python', value: 'python' }
+  ])
+  const [extensions, setextensions] = useState([javascript({ jsx: true })])
+
+  const selectChange = (value: any) => {
+    console.log('[ value ] >', value)
+    switch (value) {
+      case 'javascript':
+        setextensions([javascript({ jsx: true })])
+        break
+
+      default:
+        break
+    }
+  }
+  const onChange = useCallback((val: SetStateAction<string>, viewUpdate: any) => {
+    console.log('CodeMirror: onChange', val, viewUpdate)
     setValue(val)
   }, [])
   return (
     <>
-      <CodeMirror value={value} height="200px" extensions={[javascript({ jsx: true })]} onChange={onChange} />
+      <Select options={options} onChange={selectChange}></Select>
+      <CodeMirror value={value} height="200px" extensions={extensions} onChange={onChange} />
     </>
   )
 }
