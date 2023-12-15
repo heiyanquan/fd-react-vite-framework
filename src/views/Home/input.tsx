f3ff5f448a14e6e5e39ad098b83560b04fc58246
heiyanquan <2792652898@qq.com>
2023-12-15 11:47:39 +0800

    up

---
+++ src/views/Home/input.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HsAdminInput from '@hs-react-admin/pro-input'
+
+const App: React.FC = () => {
+  return (
+    <div>
+      Hello dumi!
+      <HsAdminInput value="Hello dumi!" />
+      <HsAdminInput value="Hello input!" />
+    </div>
+  )
+}
+
+export default App
