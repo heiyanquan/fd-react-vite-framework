2208148c7b446916ecc5b1c417253c5a3f425c74
heiyanquan <2792652898@qq.com>
2023-12-19 15:42:12 +0800

    HsAdminScrollSelect

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,27 +1,15 @@
 import React, { useEffect } from 'react'
-import { HsAdminInput } from '@react-admin/pro-components'
-import { usePage } from '@react-admin/pro-utils'
+import { HsAdminInput, HsAdminScrollSelect } from '@react-admin/pro-components'
 import { getAllUserList } from '@/api/common'
 
 const App: React.FC = () => {
-  const callList = () => {
-    return getAllUserList({
-      page: pagination.current,
-      page_size: pagination.page_size
-    }).then((res) => {
-      return res
-    })
-  }
-  const { pagination } = usePage(callList)
-
-  console.log('[ pagination ] >', pagination)
-
   useEffect(() => {}, [])
 
   return (
     <div>
       Hello dumi!
       <HsAdminInput value="Hello dumi!" />
+      <HsAdminScrollSelect request={getAllUserList} style={{ width: 240 }}></HsAdminScrollSelect>
     </div>
   )
 }
