1fc1a2db6ac464225b3a6c2e5f0f5e9bfac4ef2d
吴洋 <2792652898@qq.com>
2023-12-21 18:36:48 +0800

    HsAdminCodemirror

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,8 +1,16 @@
-import React, { useEffect } from 'react'
+import React, { SetStateAction, useEffect, useState } from 'react'
 import { HsAdminInput, HsAdminScrollSelect } from '@react-admin/pro-components'
+import { HsAdminCodemirror } from '@react-admin/pro-codemirror'
 import { getAllUserList } from '@/api/common'
 
 const App: React.FC = () => {
+  const [code, setCode] = useState('')
+
+  const codemirrorChange = (val: SetStateAction<string>, viewUpdate: any) => {
+    console.log('CodeMirror: onChange', val, viewUpdate)
+    setCode(val)
+  }
+
   useEffect(() => {}, [])
 
   return (
@@ -10,6 +18,9 @@ const App: React.FC = () => {
       Hello dumi!
       <HsAdminInput value="Hello dumi!" />
       <HsAdminScrollSelect request={getAllUserList} style={{ width: 240 }}></HsAdminScrollSelect>
+      <br />
+      <br />
+      <HsAdminCodemirror value={code} setValue={setCode} height="300px" onChange={codemirrorChange} />
     </div>
   )
 }
