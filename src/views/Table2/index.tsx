2afe1400797cf2e189d2db4329b875625673c20b
heiyanquan <2792652898@qq.com>
2024-01-15 05:50:22 +0800

    getTableHeadWidth

--- src/views/Table2/index.tsx
+++ src/views/Table2/index.tsx
@@ -26,6 +26,20 @@ const TablePage: React.FC = () => {
       dataIndex: 'description'
     }
   ])
+  const [columns2, setColumns2] = useState<any[]>([
+    {
+      title: '表编码',
+      dataIndex: 'uni_code'
+    },
+    {
+      title: '表名（中文）',
+      dataIndex: 'name'
+    },
+    {
+      title: '描述',
+      dataIndex: 'description'
+    }
+  ])
 
   const handleResize =
     (index: number) =>
@@ -36,6 +50,16 @@ const TablePage: React.FC = () => {
         width: size.width
       }
       setColumns(newColumns)
+    }
+  const handleResize2 =
+    (index: number) =>
+    (_: any, { size }: any) => {
+      const newColumns = [...columns2]
+      newColumns[index] = {
+        ...newColumns[index],
+        width: size.width
+      }
+      setColumns2(newColumns)
     }
   const mergeColumns = columns.map((col: any, index) => ({
     ...col,
@@ -49,6 +73,21 @@ const TablePage: React.FC = () => {
       return {
         width: column.width ?? 80,
         onResize: handleResize(index)
+      }
+    }
+  }))
+  const mergeColumns2 = columns2.map((col: any, index) => ({
+    ...col,
+    render: (text: string) => {
+      return (
+        //统一render
+        <div style={{ width: col.width, ...textEllipsis }}>{text}</div>
+      )
+    },
+    onHeaderCell: (column: { width: any }) => {
+      return {
+        width: column.width ?? 80,
+        onResize: handleResize2(index)
       }
     }
   }))
@@ -67,27 +106,33 @@ const TablePage: React.FC = () => {
   useEffect(() => {
     if (dataSource.length) {
       Promise.resolve().then(() => {
-        setColumns((prev) => {
-          let tableThead: any
-          const id = 'J_table1'
-          if (document.getElementById(id)) {
-            tableThead = document.querySelector(`#${id} .ant-table-thead`)
-          } else {
-            tableThead = document.getElementsByClassName('ant-table-thead')[0]
-          }
-          if (!tableThead) {
-            return prev
-          }
-          const trChildren = tableThead.children
-          const tdList = trChildren[0].children
-          for (const [key, value] of [...tdList].entries()) {
-            const computedStyles = window.getComputedStyle(value)
-            const paddingLeft = parseFloat(computedStyles['paddingLeft'])
-            const paddingRight = parseFloat(computedStyles['paddingRight'])
-            prev[key].width = value.clientWidth - paddingLeft - paddingRight
-          }
-          return [...prev]
-        })
+        const getTableHeadWidth = (
+          id: string,
+          setColumns: { (value: React.SetStateAction<any[]>): void; (value: React.SetStateAction<any[]>): void; (arg0: (prev: any) => any): void }
+        ) => {
+          setColumns((prev) => {
+            let tableThead: any
+            if (document.getElementById(id)) {
+              tableThead = document.querySelector(`#${id} .ant-table-thead`)
+            } else {
+              tableThead = document.getElementsByClassName('ant-table-thead')[0]
+            }
+            if (!tableThead) {
+              return prev
+            }
+            const trChildren = tableThead.children
+            const tdList = trChildren[0].children
+            for (const [key, value] of [...tdList].entries()) {
+              const computedStyles = window.getComputedStyle(value)
+              const paddingLeft = parseFloat(computedStyles['paddingLeft'])
+              const paddingRight = parseFloat(computedStyles['paddingRight'])
+              prev[key].width = value.clientWidth - paddingLeft - paddingRight
+            }
+            return [...prev]
+          })
+        }
+        getTableHeadWidth('J_table1', setColumns)
+        getTableHeadWidth('J_table2', setColumns2)
       })
     }
   }, [dataSource])
@@ -97,6 +142,17 @@ const TablePage: React.FC = () => {
       <Table
         id="J_table1"
         columns={mergeColumns}
+        rowKey="id"
+        dataSource={dataSource}
+        pagination={false}
+        components={{
+          header: {
+            cell: ResizableTitle
+          }
+        }}></Table>
+      <Table
+        id="J_table2"
+        columns={mergeColumns2}
         rowKey="id"
         dataSource={dataSource}
         pagination={false}
